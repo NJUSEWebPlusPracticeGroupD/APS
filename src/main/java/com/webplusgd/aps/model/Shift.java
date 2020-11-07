7bb45ec3704644bd704bff9b259ae3c3518b7507
tkTywin <tywin0302@gmail.com>
2020-11-07 20:56:23 +0800

    perf: 上传根据sql构建的实体类

---
+++ src/main/java/com/webplusgd/aps/model/Shift.java
@@ -0,0 +1,47 @@
+package com.webplusgd.aps.model;
+
+import java.util.Date;
+
+public class Shift {
+
+    private String shiftCode;
+
+    private String shiftName;
+
+    private Date startTime;
+
+    private Date endTime;
+
+    public String getShiftCode() {
+        return shiftCode;
+    }
+
+    public void setShiftCode(String shiftCode) {
+        this.shiftCode = shiftCode;
+    }
+
+    public String getShiftName() {
+        return shiftName;
+    }
+
+    public void setShiftName(String shiftName) {
+        this.shiftName = shiftName;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+}

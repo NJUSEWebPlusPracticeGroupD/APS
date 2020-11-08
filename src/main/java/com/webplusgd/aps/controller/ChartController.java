d09cd34265afbf8c4015144110e563f8e7f9b790
171250640 <171250640@smail.nju.edu.cn>
2020-11-08 17:30:20 +0800

    修改接口返回值、ApsProperties添加方法

--- src/main/java/com/webplusgd/aps/controller/ChartController.java
+++ src/main/java/com/webplusgd/aps/controller/ChartController.java
@@ -1,11 +1,15 @@
 package com.webplusgd.aps.controller;
 
+import com.webplusgd.aps.model.OrderGanttItem;
+import com.webplusgd.aps.model.ResourceGanttItem;
+import com.webplusgd.aps.model.ResourceLoadForm;
 import com.webplusgd.aps.model.ResponseVO;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 @RestController
@@ -14,17 +18,20 @@ public class ChartController {
 
     @RequestMapping(value = "/getResourceGanttChart", method = RequestMethod.GET)
     public ResponseVO getResourceGanttChart(@RequestParam Date date){
-        return null;
+
+        return ResponseVO.buildSuccess(new ArrayList<ResourceGanttItem>());
     }
 
     @RequestMapping(value = "/getOrderGanttChart", method = RequestMethod.GET)
     public ResponseVO getOrderGanttChart(@RequestParam Date date){
-        return null;
+
+        return ResponseVO.buildSuccess(new ArrayList<OrderGanttItem>());
     }
 
     @RequestMapping(value = "/getResourceLoadChart", method = RequestMethod.GET)
     public ResponseVO getResourceLoadChart(@RequestParam Date startDate){
-        return null;
+        
+        return ResponseVO.buildSuccess(new ResourceLoadForm());
     }
 
 

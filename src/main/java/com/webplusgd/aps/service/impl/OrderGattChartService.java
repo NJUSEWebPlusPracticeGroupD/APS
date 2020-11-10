fe05017be7767f0dd5a55662fc8b90ac153f3d78
tkTywin <tywin0302@gmail.com>
2020-11-10 11:15:02 +0800

    [fix]临时修复甘特图实现逻辑

--- src/main/java/com/webplusgd/aps/service/impl/OrderGattChartService.java
+++ src/main/java/com/webplusgd/aps/service/impl/OrderGattChartService.java
@@ -1,18 +1,14 @@
 package com.webplusgd.aps.service.impl;
 
 import com.webplusgd.aps.optaplanner.OptaPlanner;
-import com.webplusgd.aps.optaplanner.Planner;
 import com.webplusgd.aps.optaplanner.ScheduledTask;
 import com.webplusgd.aps.service.OrderGanttChartService;
+import com.webplusgd.aps.utils.DateUtil;
 import com.webplusgd.aps.vo.OrderGanttItem;
-import com.webplusgd.aps.vo.ResourceGanttItem;
 import com.webplusgd.aps.vo.ResponseVO;
 import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -29,22 +25,42 @@ public class OrderGattChartService implements OrderGanttChartService {
     @Override
     public ResponseVO<ArrayList<OrderGanttItem>> getOrderGanttChart(Date date) {
         try {
-            List<ScheduledTask> scheduledTasks = optaPlanner.getPlan(new Timestamp(date.getTime()));
+            List<ScheduledTask> scheduledTasks = null;
+            List<Integer> orderIds = new ArrayList<>();
             ArrayList<OrderGanttItem> orderGanttItems = new ArrayList<>();
 
-            ZoneId zoneId = ZoneId.systemDefault();
-            LocalDateTime today = date.toInstant().atZone(zoneId).toLocalDateTime();
-            LocalDateTime startDateTime = LocalDateTime.of(today.toLocalDate(), LocalTime.MIN.withHour(0));
-            LocalDateTime endDateTime = startDateTime.plusDays(1);
-
+            // 扫描排程计划表，记录所有订单 id
             for (ScheduledTask scheduledTask : scheduledTasks) {
-                if (startDateTime.isBefore(scheduledTask.getTimeslot().getStartDateTime()) && scheduledTask.getTimeslot().getEndDateTime().isBefore(endDateTime)) {
-                    Date fromDate = Date.from(scheduledTask.getTimeslot().getStartDateTime().atZone(zoneId).toInstant());
-                    int discrepantDays = (int) ((date.getTime() - fromDate.getTime()) / 1000 / 60 / 60 / 24);
-                    int progress = (discrepantDays * scheduledTask.getOrder().getProduct().getStandardCapacity()) / scheduledTask.getOrder().getOrderNum();
-                    OrderGanttItem orderGanttItem = new OrderGanttItem(Integer.toString(scheduledTask.getOrder().getOrderId()), progress, 100 - progress);
-                    orderGanttItems.add(orderGanttItem);
+                if (!orderIds.contains(scheduledTask.getOrder().getOrderId())) {
+                    orderIds.add(scheduledTask.getOrder().getOrderId());
                 }
+            }
+
+            LocalDateTime currentDate = DateUtil.date2LocalDateTime(date);
+            // 对于每个 order
+            for (Integer orderId : orderIds) {
+                int achieved = 0, goal = 1;
+                boolean firstScan = true;
+                // 遍历一遍排程计划表，累加得到截止指定日期的生产量
+                for (ScheduledTask scheduledTask : scheduledTasks) {
+                    if (orderId == scheduledTask.getOrder().getOrderId()) {
+                        if (scheduledTask.getOrder().getFinishTime().isBefore(currentDate)) {
+                            // 若排程的结束日期在指定日期之前，说明已完成，直接与排程的总生产量相加
+                            achieved += scheduledTask.getTimeslot().getDurationOfHours() * scheduledTask.getOrder().getProduct().getStandardCapacity();
+                        } else if (scheduledTask.getOrder().getStartTime().isBefore(currentDate)) {
+                            // 否则，若排程的开始日期在指定日期之前，说明正在进行但未完成，与该排程中截止指定日期的生产量相加
+                            achieved += DateUtil.hoursDiff(scheduledTask.getOrder().getStartTime(), currentDate) * scheduledTask.getOrder().getProduct().getStandardCapacity();
+                        }
+
+                        if (firstScan) {
+                            goal = scheduledTask.getOrder().getOrderNum();
+                            firstScan = false;
+                        }
+                    }
+                }
+                int progress = achieved / goal;
+                OrderGanttItem orderGanttItem = new OrderGanttItem(Integer.toString(orderId), progress, 100 - progress);
+                orderGanttItems.add(orderGanttItem);
             }
 
             return ResponseVO.buildSuccess(orderGanttItems);
--- src/main/java/com/webplusgd/aps/service/impl/ResourceGattChartService.java
+++ src/main/java/com/webplusgd/aps/service/impl/ResourceGattChartService.java
@@ -3,14 +3,12 @@ package com.webplusgd.aps.service.impl;
 import com.webplusgd.aps.optaplanner.OptaPlanner;
 import com.webplusgd.aps.optaplanner.ScheduledTask;
 import com.webplusgd.aps.service.ResourceGanttChartService;
+import com.webplusgd.aps.utils.DateUtil;
 import com.webplusgd.aps.vo.ResourceGanttItem;
 import com.webplusgd.aps.vo.ResponseVO;
 import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -27,21 +25,19 @@ public class ResourceGattChartService implements ResourceGanttChartService {
     @Override
     public ResponseVO<ArrayList<ResourceGanttItem>> getResourceGanttChart(Date date) {
         try {
-            List<ScheduledTask> scheduledTasks = optaPlanner.getPlan(new Timestamp(date.getTime()));
+            List<ScheduledTask> scheduledTasks = null;
             ArrayList<ResourceGanttItem> resourceGanttItems = new ArrayList<>();
 
-            ZoneId zoneId = ZoneId.systemDefault();
-            LocalDateTime today = date.toInstant().atZone(zoneId).toLocalDateTime();
-            LocalDateTime startDateTime = LocalDateTime.of(today.toLocalDate(), LocalTime.MIN.withHour(7));
+            // 指定日期的早晨7点
+            LocalDateTime startDateTime = DateUtil.date2LocalDateTime(date).plusHours(7);
+            // 第二天早晨7点
             LocalDateTime endDateTime = startDateTime.plusDays(1);
 
             for (ScheduledTask scheduledTask : scheduledTasks) {
                 if (startDateTime.isBefore(scheduledTask.getTimeslot().getStartDateTime()) && scheduledTask.getTimeslot().getEndDateTime().isBefore(endDateTime)) {
-                    Date fromDate = Date.from(scheduledTask.getTimeslot().getStartDateTime().atZone(zoneId).toInstant());
-                    Date toDate = Date.from(scheduledTask.getTimeslot().getEndDateTime().atZone(zoneId).toInstant());
-                    int discrepantDays = (int) ((date.getTime() - fromDate.getTime()) / 1000 / 60 / 60 / 24);
-                    boolean delay = ((discrepantDays * scheduledTask.getOrder().getProduct().getStandardCapacity()) >= scheduledTask.getOrder().getOrderNum());
-                    ResourceGanttItem resourceGanttItem = new ResourceGanttItem(scheduledTask.getResource().getName(), fromDate, toDate, Integer.toString(scheduledTask.getOrder().getOrderId()), delay);
+                    long duration = scheduledTask.getTimeslot().getDurationOfHours();
+                    boolean delay = ((duration * scheduledTask.getOrder().getProduct().getStandardCapacity()) >= scheduledTask.getOrder().getOrderNum());
+                    ResourceGanttItem resourceGanttItem = new ResourceGanttItem(scheduledTask.getResource().getName(), DateUtil.localDateTime2Date(scheduledTask.getOrder().getStartTime()), DateUtil.localDateTime2Date(scheduledTask.getOrder().getFinishTime()), Integer.toString(scheduledTask.getOrder().getOrderId()), delay);
                     resourceGanttItems.add(resourceGanttItem);
                 }
             }

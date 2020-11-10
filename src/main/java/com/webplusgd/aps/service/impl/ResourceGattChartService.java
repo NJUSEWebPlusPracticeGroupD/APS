516440919ced9e8afd7a2d7b81678f166cac6bcb
tkTywin <tywin0302@gmail.com>
2020-11-10 13:22:05 +0800

    [fix]将排程按小时切片状态下的甘特图接口

--- src/main/java/com/webplusgd/aps/service/impl/ResourceGattChartService.java
+++ src/main/java/com/webplusgd/aps/service/impl/ResourceGattChartService.java
@@ -33,13 +33,26 @@ public class ResourceGattChartService implements ResourceGanttChartService {
             // 第二天早晨7点
             LocalDateTime endDateTime = startDateTime.plusDays(1);
 
+            // 第一步：默认所有在 date 早晨7点到第二天早晨7点之间的排程未延期，添加进结果列表
             for (ScheduledTask scheduledTask : scheduledTasks) {
                 if (startDateTime.isBefore(scheduledTask.getTimeslot().getStartDateTime()) && scheduledTask.getTimeslot().getEndDateTime().isBefore(endDateTime)) {
-                    long duration = scheduledTask.getTimeslot().getDurationOfHours();
-                    boolean delay = ((duration * scheduledTask.getOrder().getProduct().getStandardCapacity()) >= scheduledTask.getOrder().getOrderNum());
-                    ResourceGanttItem resourceGanttItem = new ResourceGanttItem(scheduledTask.getResource().getName(), DateUtil.localDateTime2Date(scheduledTask.getOrder().getStartTime()), DateUtil.localDateTime2Date(scheduledTask.getOrder().getFinishTime()), Integer.toString(scheduledTask.getOrder().getOrderId()), delay);
+                    ResourceGanttItem resourceGanttItem = new ResourceGanttItem(scheduledTask.getResource().getName(), DateUtil.localDateTime2Date(scheduledTask.getOrder().getStartTime()), DateUtil.localDateTime2Date(scheduledTask.getOrder().getFinishTime()), Integer.toString(scheduledTask.getOrder().getOrderId()), false);
                     resourceGanttItems.add(resourceGanttItem);
                 }
+            }
+            // 第二步：对于当前结果列表中的所有排程，判断其是否延期
+            for (ResourceGanttItem resourceGanttItem : resourceGanttItems) {
+                LocalDateTime wholeFinishTime = startDateTime;
+                LocalDateTime termOfDeliver = startDateTime;
+                for (ScheduledTask scheduledTask : scheduledTasks) {
+                    if (resourceGanttItem.getName().equals(scheduledTask.getResource().getName()) && resourceGanttItem.getTask().equals(Integer.toString(scheduledTask.getOrder().getOrderId()))) {
+                        if (wholeFinishTime.isBefore(scheduledTask.getOrder().getFinishTime())) {
+                            wholeFinishTime = scheduledTask.getOrder().getFinishTime();
+                        }
+                        termOfDeliver = scheduledTask.getOrder().getTermOfDeliver();
+                    }
+                }
+                resourceGanttItem.setDelay(termOfDeliver.isBefore(wholeFinishTime));
             }
 
             return ResponseVO.buildSuccess(resourceGanttItems);
